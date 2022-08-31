1e4a84916d043561d93f8d61db4c6025bc2feb49
Roman Tsvietik <roman.tsvietik@marcopolonetwork.com>
2022-08-31 14:15:03 +0300

    ngIf exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <<<<<<< HEAD
-    <!--set the size of box -->
     <app-box-of-donuts
       style="border: 1px solid #000;"
-      [size]="12"
+      [size]="5"
     ></app-box-of-donuts>
   `,
 })
